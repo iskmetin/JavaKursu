747eaefeaef240ea30f14e67a382469c58daea39
metin <isk.metin.isk@gmail.com>
2020-06-05 14:50:43 +0200

    gun9

--- src/Odevler/haziran03/odev2.java
+++ src/Odevler/haziran03/odev2.java
@@ -6,8 +6,9 @@ public class odev2 {
     public static void main(String[] args) {
         //Girilen bir string içinde bir kelime arayınız, büyük harf veya küçük harf de olsa aranan kelimede bulsun
         Scanner tara=new Scanner(System.in);
-        System.out.println("2 veya 3 kelimeli ad ve soyad giriniz");
+        System.out.println("Bir cumle giriniz");
         String s1=tara.nextLine();
+        System.out.println("Aranacak kelime giriniz");
         String ara=tara.nextLine();
         System.out.println(s1.toLowerCase().contains(ara.toLowerCase ()));
 
