ad9e52bcbaa47d16e975ff122a2ec1f0d77db3d9
metin <isk.metin.isk@gmail.com>
2020-06-06 17:12:45 +0200

    gun10_3

--- src/Mentoring/haziran06Odev/KahveMakinesiCozum.java
+++ src/Mentoring/haziran06Odev/KahveMakinesiCozum.java
@@ -6,48 +6,72 @@ public class KahveMakinesiCozum {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Hangi Kahveyi İstersiniz?\n" +
-                "  1.Türk kahvesi\n" +
+        System.out.println("Hangi Kahveyi istersiniz?\n" +
+                "  1.Turk kahvesi\n" +
                 "  2.Filtre Kahve\n" +
                 "  3.Espresso");
         String hangiKahve = sc.nextLine();
-        if (hangiKahve.toUpperCase().contains("Türk kahvesi".toUpperCase())) {
-            System.out.println("Türk kahvesi hazırlanıyor.");
-        } else if (hangiKahve.toLowerCase().contains("Filtre Kahve".toLowerCase())) {
-            System.out.println("Filtre kahve hazırlanıyor.");
+        if (hangiKahve.toUpperCase().contains("Turk".toUpperCase())) {
+            System.out.println("Turk kahvesi hazirlaniyor.");
+        } else if (hangiKahve.toLowerCase().contains("Filtre".toLowerCase())) {
+            System.out.println("Filtre kahve hazirlaniyor.");
         } else if (hangiKahve.toLowerCase().contains("Espresso".toLowerCase())) {
-            System.out.println("Espresso hazırlanıyor...");
+            System.out.println("Espresso hazirlaniyor...");
         } else {
-            System.out.println("Hatalı tuşlama yaptınız.Programi bastan baslatin");
+            System.out.println("Hatali tuslama yaptiniz.Programi bastan baslatin");
             System.exit(0);
         }
 
-        System.out.println("Süt eklememizi ister misiniz ?\n" +
+        System.out.println("Sut eklememizi ister misiniz ?\n" +
                 "  1.Evet\n" +
-                "  2.Hayır\n");
+                "  2.Hayir\n");
         String sut = sc.nextLine();
         if (sut.toUpperCase().contains("Evet".toUpperCase())) {
-            System.out.println("Süt ekleniyor...");
-        } else if (hangiKahve.toUpperCase().contains("Hayır".toUpperCase())) {
-            System.out.println("Süt eklenmiyor");
+            System.out.println("Sut ekleniyor...");
+        } else if (sut.toLowerCase().contains("hayir".toLowerCase())) {
+            System.out.println("Sut eklenmiyor");
         } else {
-            System.out.println("Hatalı tuşlama yaptınız.Programi bastan baslatin");
-            System.exit(0);
-        }System.out.println("Şeker ister misiniz ? ?\n" +
-                "  1.Evet\n" +
-                "  2.Hayır\n");
-        String seker = sc.nextLine();
-        if (seker.toUpperCase().contains("Evet".toUpperCase())) {
-            System.out.println("Kaç şeker olsun?");
-            int kacSeker = sc.nextInt();
-            System.out.println(kacSeker+" şeker ekleniyor.");
-        } else if (seker.toUpperCase().contains("Hayır".toUpperCase())) {
-            System.out.println("Şeker eklenmiyor");
-
-        } else {
-            System.out.println("Hatalı tuşlama yaptınız.Programi bastan baslatin");
+            System.out.println("Hatali tuslama yaptiniz.Programi bastan baslatin");
             System.exit(0);
         }
+        System.out.println("seker ister misiniz ? ?\n" +
+                "  1.Evet\n" +
+                "  2.Hayir\n");
+        String seker = sc.nextLine();
+        if (seker.toUpperCase().contains("Evet".toUpperCase())) {
+            System.out.println("Kac seker olsun?");
+            int kacSeker = sc.nextInt();
+            System.out.println(kacSeker+" seker ekleniyor.");
+        } else if (seker.toUpperCase().contains("Hayir".toUpperCase())) {
+            System.out.println("seker eklenmiyor");
+
+        } else {
+            System.out.println("Hatali tuslama yaptiniz.Programi bastan baslatin");
+            System.exit(0);
+        }
+        System.out.println("Hangi boyutta olsun?: \n" +
+                "  1.Buyuk\n" +
+                "  2.Orta\n"+
+                "  3.kucuk\n");
+        sc.nextLine();
+        String boyut = sc.nextLine();
+
+        if (boyut.toUpperCase().contains("Buyuk".toUpperCase())) {
+            System.out.println("Kahveniz "+boyut+ " boy hazirlaniyor.");
+
+        } else if (boyut.toUpperCase().contains("Orta".toUpperCase())) {
+            System.out.println("Kahveniz "+boyut+ " boy hazirlaniyor.");
+
+        }
+        else if (boyut.toUpperCase().contains("kucuk".toUpperCase())) {
+            System.out.println("Kahveniz "+boyut+ " boy hazirlaniyor.");
+
+        }else {
+            System.out.println("Hatali tuslama yaptiniz.Programi bastan baslatin");
+            System.exit(0);
+        }
+        System.out.println( hangiKahve+" "+boyut+" boy kahveniz hazirdir. Afiyet olsun !!! ");
+
 
     }
 }
